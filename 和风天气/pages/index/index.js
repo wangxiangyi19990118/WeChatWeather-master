b24c49b87c2e6622093e4b24ad544d1cbb078357
若紫SAMA <731362676@qq.com>
2018-05-30 19:52:51 +0800

    Add files via upload

--- 和风天气/pages/index/index.js
+++ 和风天气/pages/index/index.js
@@ -1,7 +1,10 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var curCity = app.globalData.selectedCity
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.js')
+var address;
+var result;
 var list = [], result, forarray, jsonData
 var curCity = app.globalData.selectedCity//curCity为当前选择的城市，使用全局变量
 Page({
@@ -45,21 +48,30 @@ Page({
   //
   onLoad: function () {
     var that = this
-    wx.request({//初始加载天气信息，地理位置默认青岛
-      url: 'https://free-api.heweather.com/s6/weather?location=' + curCity + '&key=fd29112697d54606bd9499e54b05cf1d',//
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      success: function (res) {
-        that.setData({
-          now_weather: res.data.HeWeather6[0].now.tmp,
-          now_cond: res.data.HeWeather6[0].now.cond_txt,
-          high_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_max,
-          low_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_min,
-          for_array: res.data.HeWeather6[0].daily_forecast,
-          result: 1,
-          jsonData: JSON.stringify(res.data.HeWeather6[0].daily_forecast)
-        })
+    getGPS()
+    var times = setInterval(function () {//初始根据GPS逆定位进行请求
+      if (result) {
+        console.log('https://free-api.heweather.com/s6/weather?location=' + address + '&key=fd29112697d54606bd9499e54b05cf1d', )
+        wx.request({
+          url: 'https://free-api.heweather.com/s6/weather?location=' + address + '&key=fd29112697d54606bd9499e54b05cf1d',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          success: function (res) {
+
+            that.setData({
+              now_weather: res.data.HeWeather6[0].now.tmp,
+              now_cond: res.data.HeWeather6[0].now.cond_txt,
+              high_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_max,
+              low_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_min,
+              for_array: res.data.HeWeather6[0].daily_forecast,
+              result: 1,
+              jsonData: JSON.stringify(res.data.HeWeather6[0].daily_forecast)
+            })
+          }//加载图片，未完成
+        },
+        )
+        clearTimeout(times);//清除计数器
       }
     })
   },
@@ -68,29 +80,27 @@ Page({
   }
 })
 
-function setImage() {//加载图片，未完成
+function getGPS() {
   var that = this
-  wx.request({
-    url: 'https://free-api.heweather.com/s6/weather?location=%E9%9D%92%E5%B2%9B&key=fd29112697d54606bd9499e54b05cf1d',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+  var qqmapsdk = new QQMapWX({
+    key: 'OMFBZ-7FNKG-YBSQT-ILJ3Y-6EZ2K-WDBRU' // 必填
+  });
+  wx.getLocation({
+    type: 'wgs84',
     success: function (res) {
-
-      /*that.setData({
-        now_weather: res.data.HeWeather6[0].now.tmp,
-        now_cond: res.data.HeWeather6[0].now.cond_txt,
-        high_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_max,
-        low_tmp: res.data.HeWeather6[0].daily_forecast[0].tmp_min,
-        for_array: res.data.HeWeather6[0].daily_forecast,
-        result: 1,
-        jsonData: JSON.stringify(res.data.HeWeather6[0].daily_forecast)
-
-      })*/
-      result = 1
-      jsonData = JSON.stringify(res.data.HeWeather6[0].lifestyle)
-      //console.log(jsonData)
-      //setImage()
+      console.log(res)
+      //2、根据坐标获取当前位置名称，显示在顶部:腾讯地图逆地址解析
+      qqmapsdk.reverseGeocoder({
+        location: {
+          latitude: res.latitude,
+          longitude: res.longitude
+        },
+        success: function (addressRes) {
+          address = addressRes.result.address_component.city;
+          console.log(address)
+          result = 1;
+        }
+      })
     }
   })
 }
